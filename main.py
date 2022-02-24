4460ad756df1f9de519d71f6bc6f24838de33ffc
Qian Qian <qianq@educ.college-montreal.qc.ca>
2022-02-24 15:29:24 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Importer arcade
+import arcade
+from module.my_game import MyGame
+
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Roche, papier, ciseaux"
+
+
+def main():
+    """ Main method """
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
